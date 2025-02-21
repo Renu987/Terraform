36a9b114a2c7fbf98b99db1c90b9daffc777fdff
Renu987 <renuka.yamala2422@gmail.com>
2025-02-21 20:34:01 +0530

    Update main.tf

--- Terraform using Pipeline/main.tf
+++ Terraform using Pipeline/main.tf
@@ -1,1 +1,7 @@
-provider "aws" 
+resource "aws_instance" "webserver1" {
+  ami                    = "ami-053a45fff0a704a47"
+  instance_type          = "t2.micro"
+  }
+resource "aws_s3_bucket" "example" {
+  bucket = "renukaterraform2025project"
+}
