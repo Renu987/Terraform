9695d8e987c3d3210f07199ce97d16e015226343
Renu987 <renuka.yamala2422@gmail.com>
2025-02-21 23:27:19 +0530

    Create variables.tf

---
+++ Terraform-using-pipeline/variables.tf
@@ -0,0 +1,12 @@
+variable "ami_value" {
+    description = "value for the ami"
+}
+
+variable "instance_type_value" {
+    description = "value for instance_type"
+}
+
+
+variable "bucket_name" {
+    description = "value for the bucket_name"
+}
